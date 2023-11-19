46abb30debb9a068c95c027114867bbfd9671cca
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-11-18 23:56:27 -0300

    Correção Módulo 05 - Aula 05

--- course-01/module-05/classroom-05/Program.cs
+++ course-01/module-05/classroom-05/Program.cs
@@ -2,10 +2,10 @@
 {
     private static void Main(string[] args)
     {
-        int idade; // Recomendado iniciar com 0;
+        int idade = 0; // Recomendado iniciar com 0;
         int idade2 = 25; // Certo - inicia com o valor 25
         var idade3 = 25; // Certo - inicia com o valor 25
-        var idade4; // Errado - não foi inicializada com nenhum valor. Portanto não foi definido o tipo ainda
-        Console.WriteLine($"\nVariáveis\n\tIdade: {idade}\n\tIdade2: {idade2}\n\tIdade3: {idade3}\n\tIdade4: {idade4}\n");
+        //var idade4; // Errado - não foi inicializada com nenhum valor. Portanto não foi definido o tipo ainda
+        Console.WriteLine($"\nVariáveis\n\tIdade: {idade}\n\tIdade2: {idade2}\n\tIdade3: {idade3}\n");
     }
 }
