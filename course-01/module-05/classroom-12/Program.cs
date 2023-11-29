2bc3b6f9ff6e72051f851583654faa2e5fa301c2
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-11-29 01:50:38 -0300

    exe

---
+++ course-01/module-05/classroom-12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Classroom
+{
+        internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            short comSinalCurto = 10;
+            Console.WriteLine($"\n{comSinalCurto}\n");
+        }
+    } 
+}
+
+
