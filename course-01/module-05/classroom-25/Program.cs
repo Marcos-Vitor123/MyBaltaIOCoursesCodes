15d6c1e3df2ff54b9a9a8c9d54fd5bf5fd45f572
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-04 23:59:26 -0300

    Aula 05 - Convert

---
+++ course-01/module-05/classroom-25/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Program
+{
+   internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int inteiro = Convert.ToInt32("100");
+
+            Console.WriteLine($"\nInteiro sem conversão: {inteiro}");
+            
+            string valorTipoConvertido = inteiro.GetType().ToString();
+
+            Console.WriteLine($"\nInteiro convertido: {inteiro}\n");
+            Console.WriteLine($"Valor atual do tipo: {valorTipoConvertido}\n");
+        }
+    } 
+}
