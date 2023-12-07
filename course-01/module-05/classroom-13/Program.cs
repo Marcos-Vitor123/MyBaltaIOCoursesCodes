168c34cbec9d3331183d51ab6bd65e64c8cbbde4
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-07 00:54:25 -0300

    Alteração de endereço pasta projeto + aula 13 - modulo 05

---
+++ course-01/module-05/classroom-13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Classroom
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            float tipoFloat = 11.123456789F;
+            double tipoDouble = 22.123456789123456789;
+            decimal tipoDecimal = 33.123456789123456789123456789123456789M;
+
+            Console.WriteLine($"\nTipo float: {tipoFloat}\nTipo double: {tipoDouble}\nTipo decimal: {tipoDecimal}\n");
+        }
+    }
+}
