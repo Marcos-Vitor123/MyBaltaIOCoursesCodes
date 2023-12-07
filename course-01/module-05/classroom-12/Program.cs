710efce0f1a2a4dc738ee9384e57d454364493e4
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-07 02:01:22 -0300

    modulo 05 - aula 13

--- course-01/module-05/classroom-12/Program.cs
+++ course-01/module-05/classroom-12/Program.cs
@@ -2,14 +2,26 @@
 
 namespace Classroom
 {
-        internal class Program
+    internal class Program
     {
         private static void Main(string[] args)
         {
-            short comSinalCurto = 10;
-            Console.WriteLine($"\n{comSinalCurto}\n");
-        }
-    } 
-}
+            short tipoShortPositivo = 00012345;
+            short tipoShortNegativo = -00012345;
+            ushort tipoUShort = 000012345;
 
+            int tipoIntPositivo = 1234567891;
+            int tipoIntNegativo = -1234567891;
+            uint tipoUInt = 1234567891;
+
+            long tipoLongPositivo = 1234567891234567891;
+            long tipoLongNegativo = -1234567891234567891;
+            ulong tipoULong = 12345678912345678912;
+
+            Console.WriteLine($"\nTipo short positivo: {tipoShortPositivo}\nTipo short negativo: {tipoShortNegativo}\nTipo ushort: {tipoUShort}\n");
+            Console.WriteLine($"\nTipo int positivo: {tipoIntPositivo}\nTipo int negativo: {tipoIntNegativo}\nTipo uint: {tipoUInt}\n");
+            Console.WriteLine($"\nTipo long positivo: {tipoLongPositivo}\nTipo long negativo: {tipoLongNegativo}\nTipo ulong: {tipoULong}\n");
+        }
+    }
+}
 
--- course-01/module-05/classroom-13/Program.cs
+++ course-01/module-05/classroom-13/Program.cs
@@ -6,11 +6,18 @@ namespace Classroom
     {
         private static void Main(string[] args)
         {
-            float tipoFloat = 11.123456789F;
-            double tipoDouble = 22.123456789123456789;
-            decimal tipoDecimal = 33.123456789123456789123456789123456789M;
+            float tipoFloat = 1.234567891F; // 7 dígitos após o ponto
+            float tipoFloat2 = 1234567.891F; // Ou 7 dígitos antes do ponto
 
-            Console.WriteLine($"\nTipo float: {tipoFloat}\nTipo double: {tipoDouble}\nTipo decimal: {tipoDecimal}\n");
+            double tipoDouble = 1.234567891234567891; // 15 dígitos após o ponto
+            double tipoDouble2 = 123456789123456.789; // Ou 15 dígitos antes do ponto  
+
+            decimal tipoDecimal = 1.23456789123456789123456789123456789123M; // // 28 dígitos após o ponto
+            decimal tipoDecimal2 = 123456789123456789123456789.123456789123M; // Ou 27 dígitos antes do ponto 
+
+            Console.WriteLine($"\nTipo float: {tipoFloat}\nTipo float2: {tipoFloat2}\n");
+            Console.WriteLine($"\nTipo double: {tipoDouble}\nTipo double2: {tipoDouble2}\n");
+            Console.WriteLine($"Tipo decimal: {tipoDecimal}\nTipo decimal2: {tipoDecimal2}\n");
         }
     }
 }
