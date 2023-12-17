644821d15b680d23e9d93ea6345f7dc3b5fe3bf7
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-16 22:29:26 -0300

    module 05 - classroom 15

---
+++ course-01/module-05/classroom-15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Classroom
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            char letraA = 'a';
+            char letraE = 'e';
+            char letraI = 'i';
+            char letraO = 'o';
+            var letraU = 'u'; // Apenas para práticas
+
+            Console.WriteLine("\n\tVogais\n");
+            Console.WriteLine($"As vogais são constituídas pelas letras: {letraA}, {letraE}, {letraI}, {letraO}, {letraU}\n");
+        }
+    }
+}
---
+++ course-01/module-05/classroom-15/README.md
@@ -0,0 +1,3 @@
+# Char
+- Reserva 1 caracter apenas
+- Ocupa 19 bits de memória
