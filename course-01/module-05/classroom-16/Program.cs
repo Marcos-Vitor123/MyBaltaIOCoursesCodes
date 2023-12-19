1a5e5d84d7c59ff040e703c7401aa64dae636eea
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-19 20:19:30 -0300

    module 05 - classroom 16

--- course-01/module-05/classroom-16/Program.cs
+++ course-01/module-05/classroom-16/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Classroom
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string letra1 = "Soma";
+            string letra2 = "Arquivos";
+            var arquivo1 = 1; 
+            var arquivo2 = 2;
+            var TotalArquivos = "3";
+
+            int soma = arquivo1 + arquivo2;  
+
+            Console.WriteLine($"\n{letra1} dos {letra2}: {soma} de {TotalArquivos}\n");       
+        }
+    }
+}
