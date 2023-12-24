b634926c49c8ca5f3ff844ba890662edf6c66281
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-24 00:32:47 -0300

    Módulo 05 - Aula 19

---
+++ course-01/module-05/classroom-19/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace classroom
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int? inteiro = null;
+            Console.WriteLine(inteiro);
+
+            inteiro = 10;
+            Console.WriteLine(inteiro);
+
+            inteiro = null;
+            Console.WriteLine(inteiro);
+
+            inteiro = 0;
+            Console.WriteLine(inteiro);
+        }
+    }
+}
---
+++ course-01/module-05/classroom-19/README.md
@@ -0,0 +1,10 @@
+# Nullable Types
+- Valores nulos, ou seja sem valor nenhum
+  - Em um campo de uma variável **string** existe um espaço entre as 2 aspas, caso não haja nenhuma string e nesse caso não haveria nem isso
+  - Inesxistência de dados
+  - Ausência de qualquer dados
+- Para toranr uma variável **null**
+  - É preciso adicionar interrogaçaõ após o nome do tipo
+  ```cs
+    int? inteiro = null;
+  ```
