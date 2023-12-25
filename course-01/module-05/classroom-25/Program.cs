df0c773d765bc9af492de14c6f50e59589fbea3d
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-25 01:15:48 -0300

    Módulo 05 - Aula 25 - Entendendo um pouco mais

--- course-01/module-05/classroom-25/Program.cs
+++ course-01/module-05/classroom-25/Program.cs
@@ -7,8 +7,12 @@ namespace Program
         private static void Main(string[] args)
         {
             int inteiro = Convert.ToInt32("100");
+            string letras = Convert.ToString(100);
+            string letras2 = Convert.ToString(100.8);
 
-            Console.WriteLine($"\nValor atual do tipo inteiro para string: {inteiro}\n");
+            Console.WriteLine($"\nValor atual do tipo inteiro para string: {inteiro}");
+            Console.WriteLine($"\nValor atual do tipo string para inteiro: {letras}");
+            Console.WriteLine($"\nValor atual do tipo string para flutuante: {letras2}\n");
         }
     } 
 }
