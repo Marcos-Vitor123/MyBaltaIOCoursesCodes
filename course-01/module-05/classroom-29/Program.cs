3e87e515b154bfcdb3e1ed418be886efed59de28
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-29 05:37:18 -0300

    Módulo 05 - Aula 29

---
+++ course-01/module-05/classroom-29/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace classroom
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int x = 100;
+            Console.WriteLine($"O valor de x é: {x}");
+
+            bool result1 = x == 0;
+            Console.WriteLine($"O valor de x é: {x}, portanto o valor de comparação é: {result1}");
+
+            bool result2 = x != 0;
+            Console.WriteLine($"O valor de x é: {x}, portanto o valor de comparação é: {result2}");
+
+            bool result3 = x > 0;
+            Console.WriteLine($"O valor de x é: {x}, portanto o valor de comparação é: {result3}");
+
+            bool result4 = x < 0;
+            Console.WriteLine($"O valor de x é: {x}, portanto o valor de comparação é: {result4}");
+
+            bool result5 = x >= 0;
+            Console.WriteLine($"O valor de x é: {x}, portanto o valor de comparação é: {result5}");
+
+            bool result6 = x <= 0;
+            Console.WriteLine($"O valor de x é: {x}, portanto o valor de comparação é: {result6}");
+        }
+    }
+}
