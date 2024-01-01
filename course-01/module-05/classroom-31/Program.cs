e6b615d2efc2cd97a5e09848174041f02f9b51d1
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-31 21:56:38 -0300

    Módulo 05 - Aula 31

---
+++ course-01/module-05/classroom-31/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace classroom
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int x = 100;
+
+            bool entre = (x == 100) && (x != 80);
+            Console.WriteLine($"Resultado verdadeiro: \t{entre}");
+
+            entre = (x > 100) && (x < 100);
+            Console.WriteLine($"Resultado falso: \t{entre}");
+
+            bool ou = (x == 100) || (x < 100);
+            Console.WriteLine($"Resultado verdadeiro: \t{ou}");
+
+            ou = (x > 100) || (x < 100);
+            Console.WriteLine($"Resultado falso: \t{ou}");
+
+            bool negacao = !(x < 100);
+            Console.WriteLine($"Resultado verdadeiro: \t{negacao}");
+
+            negacao = !(x != 80);
+            Console.WriteLine($"Resultado falso: \t{negacao}");
+
+           
+            
+            
+            
+            
+            
+        }
+    }
+}
