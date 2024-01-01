a8191395a20f8c789888ebd2db8882f406bf7cb9
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-01 07:46:43 -0300

    Módulo 05 - Aula 32

---
+++ course-01/module-05/classroom-32/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace classroom
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int on = 0;
+            int off = 1;
+
+            Console.Write("Digite 0 para ligar ou 1 para desligar: ");
+            int active = Convert.ToInt32(Console.ReadLine());
+
+            if(active == on)
+            {
+                Console.WriteLine("************************************************************");
+                Console.WriteLine($"*\t\tBem vindo ao programa!                     *");
+                Console.WriteLine("*                                                          *");
+                Console.WriteLine("*                                                          *");
+                Console.WriteLine("*                                                          *");
+                Console.WriteLine("*                                                          *");
+                Console.WriteLine("*                                                          *");
+                Console.WriteLine("*                                                          *");
+                Console.WriteLine("************************************************************");
+            } else if (active == off)
+            {
+                Console.WriteLine("\nPrograma finalizado com sucesso!\n");
+            } else
+            {
+                Console.WriteLine("\nComando inválido!\n");
+            } 
+            Console.Write("Pressione qualquer tecla para sair!");
+            Console.ReadKey();
+        }
+    }
+}
