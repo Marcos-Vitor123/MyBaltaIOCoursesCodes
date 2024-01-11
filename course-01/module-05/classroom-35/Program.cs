bbecf0238ee3071668eeee85253efc0d323c6413
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-10 23:17:15 -0300

    Módulo 05 - Aula 35 - Praticando

--- course-01/module-05/classroom-35/Program.cs
+++ course-01/module-05/classroom-35/Program.cs
@@ -7,7 +7,12 @@ namespace classroom
         private static void Main(string[] args)
         {
             for (int i = 0; i < 10; i++)
-            Console.WriteLine($"{i}: {i}");
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine($"{i}\n {j}\n");
+                }
+            }
         }
     }
 }
