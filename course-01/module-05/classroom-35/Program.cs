a30cf4648ac2fed3cc3e2c3c343843af52f5ccaa
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-11 23:29:08 -0300

    Módulo 05 - Aula 35 - Gerando sequência binária de 0 a 63 em decimal

--- course-01/module-05/classroom-35/Program.cs
+++ course-01/module-05/classroom-35/Program.cs
@@ -6,11 +6,29 @@ namespace classroom
     {
         private static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 2; i++)
             {
-                for (int j = 0; j < 10; j++)
+                for (int j = 0; j < 2; j++)
                 {
-                    Console.WriteLine($"{i}\n {j}\n");
+                    for (int k = 0; k < 2; k++)
+                    {
+                        for (int l = 0; l < 2; l++)
+                        {
+                           for (int m = 0; m < 2; m++)
+                            {
+                                for (int n = 0; n < 2; n++)
+                                {
+                                    for (int o = 0; o < 2; o++)
+                                    {
+                                        for (int p = 0; p < 2; p++)
+                                        {
+                                            Console.WriteLine($"{i} {j} {k} {l} {m} {n} {o} {p}");
+                                        }
+                                    }
+                                }
+                            } 
+                        }
+                    }
                 }
             }
         }
