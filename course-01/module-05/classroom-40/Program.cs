c480c991a02eb59adaa3c20eb399409c4eca087b
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-13 23:45:50 -0300

    Módulo 05 - Aula 40 - Prática 01

---
+++ course-01/module-05/classroom-40/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace classroom
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int x = 100;
+            int y = x;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            x = 50;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+}
