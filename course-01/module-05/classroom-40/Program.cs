6ea7967c8facda61f596a072b640cf53fd9b64cd
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-13 23:53:30 -0300

    Módulo 05 - Aula 40 - Prática 02

--- course-01/module-05/classroom-40/Program.cs
+++ course-01/module-05/classroom-40/Program.cs
@@ -16,6 +16,17 @@ namespace classroom
 
             Console.WriteLine(x);
             Console.WriteLine(y);
+
+            var arr = new string[2];
+            arr[0] = "Item1";
+            var arr2 = arr;
+
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr2[0]);
+
+            arr[0] = "Item alterado";
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr2[0]);
         }
     }
 }
