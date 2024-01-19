0f454f9dac890d3592d6d819663954512f8f78f7
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-18 21:06:39 -0300

    Módulo 05 - Aula 42 - Estrutura com método construtor

--- course-01/module-05/classroom-42/Program.cs
+++ course-01/module-05/classroom-42/Program.cs
@@ -7,29 +7,33 @@ namespace Classroom
         private static void Main(string[] args)
         {
             // Criando uma estrutura
-            var product = new Product();
+            var product = new Product(1, "Mouse Gamer", 128.75f); // Passando valores as variáveis de parâmetros da função
 
-            // Criando e preechendo dados do tipo padrão
-            product.Id = 1;
-            product.Title = "Mouse Gamer";
-            product.Price = 197.23f;
-
-            Console.WriteLine(product.Id);
-            Console.WriteLine(product.Title);
-            Console.WriteLine(product.Price);
+            Console.WriteLine(product.Id); // Exibindo a propriedade Id
+            Console.WriteLine(product.Title); // Exibindo a propriedade Title
+            Console.WriteLine(product.Price); // Exibindo a propriedade Price
+            Console.WriteLine(product.priceInDolar(5.70f)); // Chamando o método ou função e passando valor via parâmetro
         }
 
         struct Product
         {
-           // Propriedades
-           public int Id;
-           public string Title;
-           public float Price;
+            // Propriedades
+            public int Id;
+            public string Title;
+            public float Price;
 
-           // Métodos 
+            // Métodos ou função
            public float priceInDolar(float dolar)
            {
                 return Price * dolar;
+           }
+
+            // Método Construtor
+           public Product(int id, string title, float price)
+           {
+                Id = id; // Definindo a variável id a propriedade Id via parâmetro da função
+                Title = title; // Definindo a variável Title a propriedade title via parâmetro da função
+                Price = price; // Definindo a variável price a propriedade Price via parâmetro da função
            }
         } 
     }
