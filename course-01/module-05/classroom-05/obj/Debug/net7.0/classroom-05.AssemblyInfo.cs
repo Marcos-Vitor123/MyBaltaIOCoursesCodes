f01f96da6b78f04f276a8c6c50edb85ee07d8900
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-01-22 22:46:36 -0300

    Módulo 05 - Aula 44

--- course-01/module-05/classroom-05/obj/Debug/net7.0/classroom-05.AssemblyInfo.cs
+++ course-01/module-05/classroom-05/obj/Debug/net7.0/classroom-05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("classroom-05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+959920e3f37fe45a508d52046b1d82e7180429a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81bee37f073697a325b87e5b7cb173599b94ca12")]
 [assembly: System.Reflection.AssemblyProductAttribute("classroom-05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("classroom-05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ course-01/module-05/classroom-44/README.md
@@ -0,0 +1,5 @@
+# Enumeradores
+- É um **inteiro** que substitui uma valor que seria usado como inteiro
+- Sempre inicia com a letra **E** maiúscula e também o nome do **emumerador** é com inicial maiúscula
+
+Continuação em breve....
