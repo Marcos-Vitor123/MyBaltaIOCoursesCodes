78a0bf956bb78f629179b658e74abe70f2ffb3d6
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-10 23:47:32 -0300

    Modulo 06 - Aula 02 subtração

---
+++ course-01/module-06/Classroom-02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Classroom 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Subtrair();
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+
+            Console.Write("Digite o primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            float resultado = valor1 + valor2;
+            Console.WriteLine($"O resultado é: {resultado}");
+
+            Console.ReadKey();
+        }
+
+        static void Subtrair()
+        {
+            Console.Clear();
+
+            Console.Write("Digite o primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            float resultado = valor1 - valor2;
+            Console.WriteLine($"O resultado é: {resultado}");
+
+            Console.ReadKey();
+        }     
+    }
+}
