6810945698d322bb339049e84f6049f5a55cc90d
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-12 22:37:37 -0300

    Modulo 07 - Aula 03 - Thread e Sleep

---
+++ course-01/module-07/Classroom-03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Classroom
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
