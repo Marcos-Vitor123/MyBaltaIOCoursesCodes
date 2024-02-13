f0ec4cd9afcca7a9fb00c49cf0428a7387fbfac0
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-12 22:25:26 -0300

    Modulo 07 - Aula 02

---
+++ course-01/module-07/Classroom-02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Classroom
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+        }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                currentTime++;
+            }
+        }
+    }
+}
