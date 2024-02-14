57db53facfcd31d32bfd31ace4a30c9d84b42cf7
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-13 21:50:39 -0300

    Modulo 07 - Aula 06

---
+++ course-01/module-07/Classroom-06/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace Classroom
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S - Segundos");
+            Console.WriteLine("M - Minutos");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower(); // Os dados vai vir maiúsculo ou minúsculo conforme o usuário digitar
+            Console.WriteLine(data);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Tempo finalizado!");
+            Thread.Sleep(2500);
+        }
+    }
+}
