cf8b3b7a72a7cd9c0e8ed92f3a5ce19d4e5f4a1e
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-27 23:08:38 -0300

    Modulo 08 - Aula 01 - Inicio do projeto

---
+++ course-01/module-08/Classroom-01/TextEditor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TextEditor
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar no arquivo");
+            Console.WriteLine("0 - Sair");
+
+            short opcao = short.Parse(Console.ReadLine());
+
+            switch(opcao)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Abrir()
+        {
+            Console.WriteLine("Abrir");
+        }
+
+        static void Editar()
+        {
+            Console.WriteLine("Editar");
+        }
+    }
+}
