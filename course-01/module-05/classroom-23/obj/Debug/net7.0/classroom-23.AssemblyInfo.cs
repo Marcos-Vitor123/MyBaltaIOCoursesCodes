0aa42fbe527ddf22f3cf474fd9e954eda0918536
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-02-28 23:57:22 -0300

    Modulo 08 - Aula 02 - Criação arquivos

--- course-01/module-05/classroom-23/obj/Debug/net7.0/classroom-23.AssemblyInfo.cs
+++ course-01/module-05/classroom-23/obj/Debug/net7.0/classroom-23.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("classroom-23")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fd19d4912d04f447130182d7e25f01d22e06d15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf8b3b7a72a7cd9c0e8ed92f3a5ce19d4e5f4a1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("classroom-23")]
 [assembly: System.Reflection.AssemblyTitleAttribute("classroom-23")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
