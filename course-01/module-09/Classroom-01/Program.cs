8bdaa0ead60d2591daea39ea597cad37ec06e595
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-03-11 20:59:48 -0300

    Modulo 09 - Aula 01 - Convertendo o hash Guid para String

--- course-01/module-09/Classroom-01/Program.cs
+++ course-01/module-09/Classroom-01/Program.cs
@@ -7,8 +7,9 @@ namespace Classroom
         private static void Main(string[] args)
         {
             var id = Guid.NewGuid();
-            id.ToString(); // Convertendo para tipo string
+            //id.ToString(); // Convertendo para tipo string
 
+            id = new Guid("23ac9f8e-8c75-4e50-9b33-476921e98f0d"); // Passagem direta como string
             Console.WriteLine(id); //Exibe números em Hash e não se repetem, é raro!
         }
     }
