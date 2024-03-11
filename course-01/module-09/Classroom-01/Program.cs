0809b8d5475e3b78a5d583c77556888bbda9e429
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-03-11 20:48:12 -0300

    Modulo 09 - Aula 01 - Criando Guid

---
+++ course-01/module-09/Classroom-01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Classroom
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var id = Guid.NewGuid();
+
+            Console.WriteLine(id);
+        }
+    }
+}
