3c1ee2da2e1b51866276302f8b49503b25500d1c
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-03-11 21:06:18 -0300

    Modulo 09 - Aula 01 - Exibindo somente a 8 primeiras posições do Hash

--- course-01/module-09/Classroom-01/Program.cs
+++ course-01/module-09/Classroom-01/Program.cs
@@ -9,8 +9,9 @@ namespace Classroom
             var id = Guid.NewGuid();
             //id.ToString(); // Convertendo para tipo string
 
-            id = new Guid("23ac9f8e-8c75-4e50-9b33-476921e98f0d"); // Passagem direta como string 
-            Console.WriteLine(id); //Exibe números em Hash e não se repetem, é raro!
+            //id = new Guid("23ac9f8e-8c75-4e50-9b33-476921e98f0d"); // Passagem direta como string 
+
+            Console.WriteLine(id.ToString().Substring(0, 8)); //Exibe números em Hash e não se repetem, é raro!
         }
     }
 }
