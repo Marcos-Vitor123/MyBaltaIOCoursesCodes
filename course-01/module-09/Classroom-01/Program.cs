5b32e14e996fea380546121385b084b0d3be9102
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-03-11 21:13:34 -0300

    Modulo 09 - Aula 01 - Comparando 2 Guid de diferentes geração

--- course-01/module-09/Classroom-01/Program.cs
+++ course-01/module-09/Classroom-01/Program.cs
@@ -10,8 +10,16 @@ namespace Classroom
             //id.ToString(); // Convertendo para tipo string
 
             //id = new Guid("23ac9f8e-8c75-4e50-9b33-476921e98f0d"); // Passagem direta como string 
-
-            Console.WriteLine(id.ToString().Substring(0, 8)); //Exibe números em Hash e não se repetem, é raro!
+            if(id == Guid.NewGuid()) // Comparando 2 Guid de diferentes geração
+            {
+                Console.WriteLine("Guid's Iguais!");
+                Console.WriteLine(id); 
+            } else
+            {
+                Console.WriteLine(id);
+            }
+            //Exibe números em Hash e não se repetem, é raro!
+            //Console.WriteLine(id.ToString().Substring(0, 8)); // Exibindo somente a 8 primeiras posições do Hash
         }
     }
 }
