fc36ca1338b781e327bba206681498d061d741d2
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-03-12 22:45:37 -0300

    Modulo 09 - Aula 02 - Método Contains

--- course-01/module-09/Classroom-02/Program.cs
+++ course-01/module-09/Classroom-02/Program.cs
@@ -7,8 +7,13 @@ namespace Classroom
         private static void Main(string[] args)
         {
             var texto = "Testando";
-            Console.WriteLine(texto.CompareTo("Testando")); // CompareTo() => É uma extensão de tipos strings
+            Console.WriteLine(texto.CompareTo("Testando")); // CompareTo() => É uma extensão de tipos strings para comparação de 2 string totalmente iguais
             Console.WriteLine(texto.CompareTo("testando"));
+
+            var texto2 = "Eu gosto de Csharp";
+            Console.WriteLine(texto2.Contains("Csharp")); // Contains() => Método para comparar uma string inteira ou parte dela
+            Console.WriteLine(texto2.Contains("eu"));
+            Console.WriteLine(texto2.Contains("eu", StringComparison.OrdinalIgnoreCase));
         }
     }
 }
