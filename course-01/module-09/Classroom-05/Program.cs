0c977e94bd2d8cfe08f0738ed3db081cd67d6289
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-04-07 21:42:17 -0300

    LastIndexOf

--- course-01/module-09/Classroom-05/Program.cs
+++ course-01/module-09/Classroom-05/Program.cs
@@ -8,7 +8,8 @@ namespace Classroom
         {
             var texto = "CSharp é o máximo";
 
-            Console.WriteLine(texto.IndexOf("é"));
+            Console.WriteLine(texto.IndexOf("m")); // Exibe a posição do primeiro caracter qualquer escolhido na frase ou array
+            Console.WriteLine(texto.LastIndexOf("m")); // Exibe a última posição de um caracter escolhido na frase ou array
         }
     }
 }
