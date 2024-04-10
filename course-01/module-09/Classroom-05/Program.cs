089af286ef033c08238c313e60fb93611dc4d516
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-04-09 23:53:51 -0300

    Novos teste de prática

--- course-01/module-09/Classroom-05/Program.cs
+++ course-01/module-09/Classroom-05/Program.cs
@@ -10,6 +10,7 @@ namespace Classroom
 
             Console.WriteLine(texto.IndexOf("m")); // Exibe a posição do primeiro caracter qualquer escolhido na frase ou array
             Console.WriteLine(texto.LastIndexOf("m")); // Exibe a última posição de um caracter escolhido na frase ou array
+            Console.WriteLine(texto.LastIndexOf("C"));
         }
     }
 }
