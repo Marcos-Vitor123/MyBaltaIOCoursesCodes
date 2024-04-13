583b1f46273c4bd9139504c6103e29e40d4d4d5a
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-04-12 21:54:24 -0300

    Finalizando a aula 06 modeulo 09

--- course-01/module-09/Classroom-06/Program.cs
+++ course-01/module-09/Classroom-06/Program.cs
@@ -7,7 +7,10 @@
             var texto = "CSharp é o máximo";
 
             Console.WriteLine(texto.ToUpper());
-            Console.WriteLine(texto.ToLower()); 
+            Console.WriteLine(texto.ToLower());
+            Console.WriteLine(texto.Insert(9, "mais que ")); // Insere um caracter na posição indicada pelo primeiro parâmetro
+            Console.WriteLine(texto.Remove(7, 10));
+            Console.WriteLine(texto.Length); // Propriedade
         }
     }
 }
