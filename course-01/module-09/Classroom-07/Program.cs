dcb237a48fe1b04213663a87ac196a04fec139ee
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-04-15 20:12:58 -0300

    Aula 07 - Método Split

--- course-01/module-09/Classroom-07/Program.cs
+++ course-01/module-09/Classroom-07/Program.cs
@@ -9,6 +9,12 @@ namespace Classroom
             var texto = "CSharp é o máximo";
             Console.WriteLine(texto.Replace("CSharp", "C#"));
             Console.WriteLine(texto.Replace("o", "X"));
+
+            var divisao = texto.Split(" ");
+            Console.WriteLine(divisao[0]);
+            Console.WriteLine(divisao[1]);
+            Console.WriteLine(divisao[2]);
+            Console.WriteLine(divisao[3]);
         }
     }
 }
