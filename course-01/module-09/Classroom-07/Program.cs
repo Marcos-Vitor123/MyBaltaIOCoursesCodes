2acbdc75632e6e6908cfb9a26188d50a9beebc43
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-04-14 23:02:24 -0300

    Aula 07 - Método Replace

---
+++ course-01/module-09/Classroom-07/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Classroom 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var texto = "CSharp é o máximo";
+            Console.WriteLine(texto.Replace("CSharp", "C#"));
+        }
+    }
+}
