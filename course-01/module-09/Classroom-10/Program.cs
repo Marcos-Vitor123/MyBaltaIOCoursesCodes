bf27aa485737f5a0cf1cec39750e08a9bbaced2e
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-04-16 21:50:57 -0300

    Aula 10 - Método StringBuilder

---
+++ course-01/module-09/Classroom-10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace Classroom 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var texto = new StringBuilder();
+
+            texto.Append("CSharp é o máximo,");
+            texto.Append(" pois ");
+            texto.Append("é minha ");
+            texto.Append("primeira linguagem");
+
+            texto.ToString();
+            Console.WriteLine(texto);
+        }
+    }
+}
