4eec15d28e5b0e4d1cd212d978adf8ec856d7a6f
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-04-17 22:15:38 -0300

    Modulo 10 - Aula 01: Iniciando projeto EditorHtml

---
+++ course-01/module-10/Classroom-01/EditorHtml/Menu.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace EditorHtml
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+        }
+    }
+}
