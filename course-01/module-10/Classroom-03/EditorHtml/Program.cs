48042f9057a868496554be6168fef8d75587cbea
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2024-04-20 22:20:05 -0300

    Modulo 10 - Aula 03: Criando menu

---
+++ course-01/module-10/Classroom-03/EditorHtml/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EditorHtml
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Menu.Show();
+        }
+    }
+}
